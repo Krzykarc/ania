94e2b37ad1b8fe90305053d1a4d06210276862d6
Krzykarc <krzykarc@gmail.com>
2024-04-12 14:19:07 +0200

    Updates

---
+++ assets/index.529c2aa8.js
@@ -0,0 +1,1 @@
+import{q as e,o as n,c as t,t as o}from"./entry.6ca4037d.js";const _=2+3,r=e({__name:"index",setup(s){return(a,c)=>(n(),t("div",null," Hello Nuxt "+o(_)+"! xd "))}});export{r as default};
