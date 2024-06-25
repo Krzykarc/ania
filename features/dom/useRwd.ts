cf705ebb48dd13c4803a83693b5fa171a1074030
KrzysiekIT <72068172+KrzysiekIT@users.noreply.github.com>
2024-06-25 23:57:24 +0200

    fix width

--- features/dom/useRwd.ts
+++ features/dom/useRwd.ts
@@ -1,7 +1,7 @@
 const MOBILE_BREAKPOINT_PX: Readonly<number> = 768;
 
 export const useRwd = () => {
-  const screenWidth: Ref<number> = ref(0);
+  const screenWidth: Ref<number> = ref(window.innerWidth);
 
   const setWidth = () => {
     screenWidth.value = window.innerWidth;
